ad8b39832eee3722c2b667200841e47e7044dd57
Shaoze Yang <priest-yang@sjtu.edu.cn>
2023-09-26 14:41:15 -0400

    tutorial 4 for autograder

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -14,9 +14,10 @@ urlpatterns = [
     # path("<int:question_id>/vote/", views.vote, name="vote"),
     # path('owner', views.owner, name='owner'),
 
-    path("", views.IndexView.as_view(), name="index"),
-    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
-    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
-    path("<int:question_id>/vote/", views.vote, name="vote"),
+    path('', views.IndexView.as_view(), name='index'),
+    path('owner', views.owner, name='owner'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
 
 ]
